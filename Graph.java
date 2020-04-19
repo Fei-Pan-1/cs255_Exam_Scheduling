09ac5392fd7963c9b5f28f0035d84b79e29e904a
Joshua Benz <joshua@planethull.com>
2020-04-19 16:23:07 -0400

    added another class

--- Graph.java
+++ Graph.java
@@ -2,9 +2,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Graph {
-	private List<List<EdgeNode>> adjList;
+	private List<VertexNode> adjList;
 	public int nVertices;
-	private int nEdges;
 
 	private class EdgeNode {
 		public int source;
@@ -20,41 +19,60 @@ public class Graph {
 		}
 	}
 
-	public Graph(int n) {
-		nVertices = n;
-		adjList = new ArrayList<List<EdgeNode>>(n);
+	private class VertexNode {
+		public int vertex;
+		public List<EdgeNode> edges;
 
-		for(int i=0; i<n; i++) {
-			adjList.add(new ArrayList<EdgeNode>());
+		public VertexNode(int v) {
+			vertex = v;
+			edges = new ArrayList<EdgeNode>();
+		}
+
+		public void addEdge(EdgeNode e) {
+			edges.add(e);
+		}
+
+		public boolean contains(int target) {
+			for(EdgeNode edge : edges) {
+				if(edge.target == target) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public String toString() {
+			String result = new String(vertex + ": ");
+			for(EdgeNode edge : edges) {
+				result = result + edge.target + " ";
+			}
+			return result;
 		}
 	}
 
-	public boolean vertexContains(int source, int target) {
-		List<EdgeNode> edges = adjList.get(source);
-		for(EdgeNode edge: edges) {
-			if(edge.target == target) {
-				return true;
-			}
+	public Graph(int n) {
+		nVertices = n;
+		adjList = new ArrayList<VertexNode>(n);
+
+		for(int i=0; i<n; i++) {
+			adjList.add(new VertexNode(i));
 		}
-		return false;
 	}
 
 	public void addEdge(int source, int target) {
-		adjList.get(source).add(new EdgeNode(source, target));
+		adjList.get(source).addEdge(new EdgeNode(source, target));
+	}
+
+	public VertexNode vertex(int v) {
+		return adjList.get(v);
 	}
 
 	public String toString() {
-		int vertex = 0;
 		String result = new String();
-
-		for(List<EdgeNode> edges: adjList) {
-			result =  result + vertex + ":";
-			for(EdgeNode edge: edges) {
-				result = result + edge.toString();
-			}
-			vertex++;
-			result += "\n";
+		for(VertexNode vertex : adjList) {
+			result = result + vertex.toString() + "\n";
 		}
+
 		return result;
 	}
 
@@ -68,8 +86,8 @@ public class Graph {
 					Math.random() < probabilityThreshold) {
 					//since the graph is undirected, we shouldn't add edges
 					//if there is already an edge
-					if(!graph.vertexContains(v1, v1) &&
-						!graph.vertexContains(v2, v1)) {
+					if(!graph.vertex(v1).contains(v2) &&
+						!graph.vertex(v2).contains(v1)) {
 						graph.addEdge(v1, v2);
 						graph.addEdge(v2, v1);
 
@@ -78,6 +96,10 @@ public class Graph {
 			}
 		}
 		return graph;
+	}
+
+	public static void WelshPowell(Graph g) {
+
 	}
 
 	public static void main(String[] args) {
