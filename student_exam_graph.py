66367eaf9981099ee48bb9c688480ba50b4209bd
Fei Pan <fei.pan@sjsu.edu>
2020-04-24 12:05:43 -0700

    write all edges to edges.csv

--- student_exam_graph.py
+++ student_exam_graph.py
@@ -16,21 +16,22 @@ print(dmap)
 edges = []
 for key in dmap:
     exams_each_student = list(dmap[key])
-    print(exams_each_student)
+    # print(exams_each_student)
     if len(exams_each_student) >= 2:
         lst = list(combinations(exams_each_student, 2))
         for l in lst:
             edges.append(l)
 
 print(len(edges), "edges: ", edges)
+df_edges = pd.DataFrame(edges, columns=['exam1', 'exam2'])
+print(df_edges)
+df_edges.to_csv('dataset/processed_data/edges.csv', index=False)
 
 graph = Graph()
 for e in edges:
     u_s, v_s = e
     u = Vertex(u_s)
     v = Vertex(v_s)
-    # u.add_neighbor(v)
-    # v.add_neighbor(u)
     # graph.add_vertex(u)
     # graph.add_vertex(v)
     # graph.add_edge(u, v)
