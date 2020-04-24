132fa24e192a7bf0dec8f1dac84470590321bb8c
Fei Pan <fei.pan@sjsu.edu>
2020-04-24 01:44:58 -0700

    ready to genarate graph from enrolments_with_id.csv

--- student_exam_graph.py
+++ student_exam_graph.py
@@ -1,30 +1,14 @@
-import csv
-import fileinput
+
+# put each student's exams in a set.
 import pandas as pd
 
-# covert txt file to csv file (add ',')
-input_file = 'dataset/enrolments.txt'
-output = open('dataset/enrolments_out.txt', 'w+')
-'''
-input_file = 'testdata/enrolments_test.txt'
-output = open('testdata/enrolments_test_out.txt', 'w+')
-'''
-with output as f:
-    for line in fileinput.input(input_file):
-        line = line.split(None,2)
-        f.write(','.join(line))
-        f.write('\n')
-read_file = pd.read_csv (r'dataset/enrolments_out.txt')
-read_file.to_csv (r'dataset/enrolments.csv', index=None)
-
-# put one student's exams in a set.
-df = pd.read_csv('dataset/enrolments.csv')
-df = df.sort_values(by=['Student'])
+df = pd.read_csv('dataset/processed_data/enrolments_with_id.csv')
+df = df.drop(['student','exam'], axis=1)
+print(df)
 dmap = {}
 for row in df.values:
     dmap.setdefault(row[0], set()).add(row[1])
-#print(dmap)
-#print(df)
+print(dmap)
 
-# TO_DO: draw complete graph for each set. Or list all combinations of 2 in each set as edges.
+# TO_DO: list all combinations of 2 in each set as edges.
 
