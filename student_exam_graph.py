96307c0c5fcb4d0dd1ed5d63944484bfa2617c46
Fei Pan <fei.pan@sjsu.edu>
2020-04-24 02:05:14 -0700

    clean code

--- student_exam_graph.py
+++ student_exam_graph.py
@@ -1,7 +1,7 @@
-
-# put each student's exams in a set.
+from itertools import combinations
 import pandas as pd
 
+# put each student and his exams in a dictionary.
 df = pd.read_csv('dataset/processed_data/enrolments_with_id.csv')
 df = df.drop(['student','exam'], axis=1)
 print(df)
@@ -10,5 +10,12 @@ for row in df.values:
     dmap.setdefault(row[0], set()).add(row[1])
 print(dmap)
 
-# TO_DO: list all combinations of 2 in each set as edges.
+# list all combinations of 2 in each set as edges.
+edges = []
+for key in dmap:
+    exams_each_student = list(dmap[key])
+    # print(exams_each_student)
+    lst = list(combinations(exams_each_student, 2))
+    edges.append(lst)
 
+print(len(edges), "edges: ", edges)
