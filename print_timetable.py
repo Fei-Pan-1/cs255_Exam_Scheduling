aa7e0e4d6a574a35a1d3c47644aae9c890711705
Fei Pan <fei.pan@sjsu.edu>
2020-04-27 16:13:01 -0700

    Sorted timetable by department

--- print_timetable.py
+++ print_timetable.py
@@ -17,6 +17,7 @@ for index, row in df_solution.iterrows():
     nextRoom[row['tid']] = roomNo + 1
 print(assigned_room)
 
+# add room column to solution
 df_solution['rid'] = assigned_room
 print(df_solution)
 # df_solution.to_csv('dataset/results/solution.csv', index=False)
@@ -27,9 +28,10 @@ df_timetable = pd.merge(df_timetable_draft, df_timeslots, on='tid')
 df_timetable = df_timetable.sort_values('eid').reset_index(drop=True)
 df_timetable = df_timetable.drop(columns=['duration','tid'], axis=1)
 # rename the columns
-df_timetable.columns = ['Exam Id','Exam Name','Description','Department','Room','Start Time','End Time','Date']
+df_timetable.columns = ['Exam Id','Exam','Description','Department','Room','Start Time','End Time','Date']
 # reorder the columns
-df_timetable = df_timetable[['Exam Id','Exam Name','Description','Department','Start Time','End Time','Date','Room']]
+df_timetable = df_timetable[['Exam Id','Exam','Description','Department','Start Time','End Time','Date','Room']]
+df_timetable = df_timetable.sort_values('Department').reset_index(drop=True)
 print(df_timetable)
 
 df_timetable.to_csv('dataset/results/timetable.csv', index=False)
