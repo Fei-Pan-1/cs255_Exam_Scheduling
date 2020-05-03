baec8aeed57f21a755f695d9c07e8c1705d2a3d8
Joshua Benz <joshua@planethull.com>
2020-05-02 21:15:07 -0400

    more info

--- exam_graph.py
+++ exam_graph.py
@@ -44,6 +44,8 @@ print('Greedy Solution for Exams: \n',coloring, '\n', max(coloring.values()) + 1
 # print(str(len(g.vertices)))
 coloring1 = Graph.welsh_powell(graph)
 print('Welsh Powell Solution for Exams: \n', coloring1, '\n', max(coloring1.values()) + 1, ' colors used.')
+
+print('Beginning Genetic Algorithm: \n')
 used, genetic_coloring = Graph.genetic_algorithm(graph, 38)
 print('Genetic Algorithm Solution for Exams: \n',genetic_coloring, '\n', used, ' colors used.')
 
